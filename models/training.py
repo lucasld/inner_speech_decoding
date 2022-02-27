370a433716031d328c426b44b5e29b0d76621213
lucasld <lucas.ld@hermes-link.de>
2022-02-27 14:04:28 +0100

    added possibility to change accurarcy-calculation, added training-progress-visualisation

--- models/training.py
+++ models/training.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
+import utilities
 import numpy as np
 
 
-def train_step(model, input, target, loss_function, optimizer):
+def train_step(model, input, target, loss_function, optimizer, acc_type):
     """Apply optimizer to all trainable variables of a model to
     minimize the loss (loss_function) between the target output and the
     predicted ouptut.
@@ -26,11 +27,11 @@ def train_step(model, input, target, loss_function, optimizer):
     gradients = tape.gradient(loss, model.trainable_variables)
     # apply gradients to the trainable variables using a optimizer
     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-    accuracy = arg_max_accuracy(prediction, target)
-    return loss, accuracy
+    acc = accuracy(prediction, target, acc_type)
+    return loss, acc
 
 
-def test(model, test_data, loss_function):
+def test(model, test_data, loss_function, acc_type):
     """Calculate the mean loss and accuracy of the model over all elements
     of test_data.
     :param model: model to train
@@ -49,17 +50,17 @@ def test(model, test_data, loss_function):
     for (input, target) in test_data:
         prediction = model(input)
         loss = loss_function(target, prediction)
-        accuracy = arg_max_accuracy(prediction, target)
+        acc = accuracy(prediction, target, acc_type)
         # add loss and accuracy to aggregators
         test_loss_agg.append(loss.numpy())
-        test_accuracy_agg.append(np.mean(accuracy))
+        test_accuracy_agg.append(np.mean(acc))
     # calculate mean loss and accuracy
     test_loss = tf.reduce_mean(test_loss_agg)
     test_accuracy = tf.reduce_mean(test_accuracy_agg)
     return test_loss, test_accuracy
 
 
-def arg_max_accuracy(pred, target):
+def accuracy(pred, target, type):
     """Calucalte accuracy between a prediction and a target.
     
     :param pred: a prediction that the model made
@@ -67,11 +68,16 @@ def arg_max_accuracy(pred, target):
     :param target: target that model should have predicted
     :type target: tf.Tensor of floats
     """
-    same_prediction = np.argmax(target, axis=1) == np.argmax(pred, axis=1)
-    return np.mean(same_prediction)
+    if type == 'arg_max':
+        same_prediction = np.argmax(target, axis=1) == np.argmax(pred, axis=1)
+        acc = np.mean(same_prediction)
+    elif type == 'mse':
+        target = tf.cast(target, tf.float32)
+        acc = tf.math.reduce_mean(tf.math.square(tf.math.subtract(target, pred)))
+    return acc
 
 
-def training(input_model, datasets, loss_function, optimizer, epochs=10):
+def training(input_model, datasets, loss_function, optimizer, acc_type, epochs=10):
     """Training a model on a dataset for a certain amount of epochs.
     :param input_model: model to be trained
     :type input_model: model of type CustomModel
@@ -95,26 +101,28 @@ def training(input_model, datasets, loss_function, optimizer, epochs=10):
     accuracies = {'train':[], 'valid':[], 'test':0}
         
     # Train-Dataset
-    train_loss, train_accuracy = test(input_model, datasets['train'], loss_function)
+    train_loss, train_accuracy = test(input_model, datasets['train'], loss_function, acc_type)
 
     losses['train'].append(train_loss)
     accuracies['train'].append(train_accuracy)
 
-    valid_loss, valid_accuracy = test(input_model, datasets['valid'], loss_function)
+    valid_loss, valid_accuracy = test(input_model, datasets['valid'], loss_function, acc_type)
     #valid_losses.append(valid_loss)
     #valid_accuracies.append(valid_accuracy)
     losses['valid'].append(valid_loss)
     accuracies['valid'].append(valid_accuracy)
-
+    # grapher
+    graphs = utilities.TrainingGrapher(2, 1, name="Training Progress", supxlabel='Epochs', x_scale=[[[0, epochs], [0, epochs]]])
     # Training for epochs
     for epoch in range(1, epochs+1):
         last_valid_acc = np.round(accuracies['valid'][-1], 3)
-        print(f"Epoch {str(epoch)} starting with validation accuracy of {last_valid_acc} and validation loss of {losses['valid'][-1]:.4f}")
+        graphs.update([range(epoch) for _ in range(2)], [accuracies['train'], losses['train']])
+        #print(f"Epoch {str(epoch)} starting with validation accuracy of {last_valid_acc} and validation loss of {losses['valid'][-1]:.4f}")
         epoch_loss_agg = []
         epoch_accuracy_agg = []
         for input, target in datasets['train']:
             train_loss, train_accuracy = train_step(
-                input_model, input, target, loss_function, optimizer
+                input_model, input, target, loss_function, optimizer, acc_type
             )
             epoch_loss_agg.append(train_loss)
             epoch_accuracy_agg.append(train_accuracy)
@@ -122,10 +130,10 @@ def training(input_model, datasets, loss_function, optimizer, epochs=10):
         losses['train'].append(tf.reduce_mean(epoch_loss_agg))
         accuracies['train'].append(tf.reduce_mean(epoch_accuracy_agg))
         # track loss and accuracy for test-dataset
-        valid_loss, valid_accuracy = test(input_model, datasets['valid'], loss_function)
+        valid_loss, valid_accuracy = test(input_model, datasets['valid'], loss_function, acc_type)
         losses['valid'].append(valid_loss)
         accuracies['valid'].append(valid_accuracy)
-    test_loss, test_accuracy = test(input_model, datasets['test'], loss_function)
+    test_loss, test_accuracy = test(input_model, datasets['test'], loss_function, acc_type)
 
     losses['test'] = test_loss
     accuracies['test'] = test_accuracy    
