685544bb918fb3c2b786c49d6c4af6a8355839bf
lucasld <lucas.ld@hermes-link.de>
2022-03-27 13:24:13 +0200

    ...

--- classify.py
+++ classify.py
@@ -260,6 +260,19 @@ if __name__ == '__main__':
         if name == '-k': KERNEL_LENGTH = int(arg)
         if name == '-n': N_CHECKS = int(arg)
         if name == '-b': BATCH_SIZE = int(arg)
+    
+    gpus = tf.config.list_physical_devices('GPU')
+    if gpus:
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
+
     for subject in SUBJECT_S:
         # option 1: execute code with extra process
         #p = multiprocessing.Process(target=subject_train_test_average, args=(subject, EPOCHS, DROPOUT, KERNEL_LENGTH, BATCH_SIZE, N_CHECKS))
