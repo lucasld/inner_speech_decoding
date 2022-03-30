a8f2191a2b42700e0c7f37eec875fa13123938ce
lucasld <lucas.ld@hermes-link.de>
2022-03-30 20:00:43 +0200

    added pretrain functionality

--- subject_independent.py
+++ subject_independent.py
@@ -57,7 +57,6 @@ def no_pretrain_inner_speech():
     path = './models/saved_models/no_pretrain_inner_speech'
     model.save(path)
     del model
-
     ###### DATA
     data, events = dp.load_data()
     # shuffle data and labels
@@ -74,10 +73,92 @@ def no_pretrain_inner_speech():
     data = scipy.stats.zscore(data, axis=1)
     # reshape
     data = data.reshape(*data.shape, 1)
-    
     ###### KFOLD TRAINING
     kfold_training(data, events, path)
 
+
+def pretrain_non_inner_speech():
+    """This function aims at pretraining a model on pronounced speech and
+    visualized condition data and then evaluating the effectiveness of transfer
+    learning to the inner speech condition (k-fold)
+
+    :return: metric history for every of the n k-folds and the pretraining
+    :rtype: two lists of dictonaries   
+    """
+    # load all data
+    data, events = dp.load_data()
+    # shuffle data and labels
+    data, events = sklearn.utils.shuffle(data, events)
+     # save memory by converting from 64bit to 32bit floats
+    data = data.astype(np.float32)
+    # reshape
+    data = data.reshape(*data.shape, 1)
+    # pretrain data
+    data_pronounced_speech, events_pronounced_speech = dp.choose_condition(data, events, 'pronounced speech')
+    data_visualized_cond, events_visualized_cond = dp.choose_condition(data, events, 'visualized condition')
+    data_pretrain = np.concatenate([data_pronounced_speech, data_visualized_cond], axis=0)
+    events_pretrain = np.concatenate([events_pronounced_speech, events_visualized_cond], axis=0)
+    # select the column containing directions (up, down, left, right)
+    events_pretrain = events_pretrain[:, 1]
+    # one-hot event data 
+    events_pretrain = np_utils.to_categorical(events_pretrain, 4)
+    # zscore normalize the data
+    data_pretrain = scipy.stats.zscore(data_pretrain, axis=1)
+    # inner speech data
+    data_inner_speech, events_inner_speech = dp.choose_condition(data, events, 'inner speech')
+    # convert to tf.Dataset
+    dataset_pretrain = tf.data.Dataset.from_tensor_slices((data_pretrain, events_pretrain))
+    dataset_inner_speech = tf.data.Dataset.from_tensor_slices((data_inner_speech, events_inner_speech))
+
+    ###### PRETRAINING
+    print("Pretraining...")
+    tf.debugging.set_log_device_placement(True)
+    gpus = tf.config.list_logical_devices('GPU')
+    # tensorflows mirrored strategy adds support to do synchronous distributed
+    # training on multiple GPU's
+    mirrored_strategy = tf.distribute.MirroredStrategy(gpus)
+    with mirrored_strategy.scope():
+        # create EEGNet (source: https://github.com/vlawhern/arl-eegmodels)
+        model_pretrain = EEGNet(nb_classes=4, Chans=data.shape[1],
+                                Samples=data.shape[2], dropoutRate=DROPOUT,
+                                kernLength=KERNEL_LENGTH, F1=8, D=2, F2=16, dropoutType='Dropout')
+        # adam optimizer
+        optimizer = tf.keras.optimizers.Adam()
+    # compile model
+    model_pretrain.compile(loss='categorical_crossentropy',
+                            optimizer=optimizer,
+                            metrics=['accuracy'])
+    # fit model to pretrain data
+    pretrain_history = model_pretrain.fit(data_pretrain, epochs=PRETRAIN_EPOCHS,
+                                          verbose=1, validation_data=dataset_inner_speech)
+    # save pretrained model so it can be used for transfer learning
+    path = './models/saved_models/pretrained_subject_independent'
+    model_pretrain.save(path)
+    del model_pretrain
+    print("Pretraining Done")
+
+    ###### TRANSFER LEARNING
+    history_accumulator = []
+    for n in range(N_CHECKS):
+        # kfold testing of transfer learning
+        k_history = kfold_training(data_inner_speech, events_inner_speech, path)
+        # add kfold metric-history
+        history_accumulator += k_history
+        print("\n\nN: ", n, "     ######################\n")
+        print("Mean for K Folds:", np.mean([h['val_accuracy'][-1] for h in k_history]))
+        print("New Total Mean:", np.mean([h['val_accuracy'][-1] for h in history_accumulator]))
+    
+    # save progress
+    print("Parameter Test Done")
+    print("Average Accuracy:", np.mean([h['val_accuracy'][-1] for h in history_accumulator]))
+    print("Parameters")
+    print("EPOCHS:", EPOCHS)
+    print("DROPOUT", DROPOUT)
+    print("KERNEL_LENGTH", KERNEL_LENGTH)
+    print("N_CHECKS", N_CHECKS)
+    print("BATCH SIZE", BATCH_SIZE)
+    print(history_accumulator)
+    return pretrain_history.history, history_accumulator
 
 
 if __name__ == '__main__':
@@ -118,7 +199,11 @@ if __name__ == '__main__':
             # Memory growth must be set before GPUs have been initialized
             print(e)
     if MODE == 'no_pretrain':
-        history = no_pretrain_all_data()
+        print("NO PRETRAINING!")
+        history = no_pretrain_inner_speech()
+    elif MODE == 'pretrain':
+        print("PRETRAINING")
+        pretrain_history, history = pretrain_non_inner_speech()
     # plot all subject's inter-training results
     f = open(f'./{title}/results.txt', 'a')
     f.write(f"\n\n{history}")
